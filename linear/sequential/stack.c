70b66a407aa59743140d29214cac9375b54e20cd
zulqarnainchishti <thisiszulqarnain@gmail.com>
2025-06-15 16:57:30 +0530

    new stack

--- linear/sequential/stack.c
+++ linear/sequential/stack.c
@@ -2,103 +2,99 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct{
+    int *array;
+    int top;
+    int size;
+}Stack;
+
 void swap(int *a, int *b){
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-typedef struct{
-    int size;
-    int *array;
-    int top;
-}Stack;
-
 Stack init(int n){
     Stack stack;
-    stack.size=n;
     stack.array=(int *)malloc(n*sizeof(int));
-    stack.top=-1;
+    stack.top=0;
+    stack.size=n;
     return stack; 
 }
 
 Stack copy(Stack stack){
     Stack result=init(stack.size);
-    for (int i = 0; i < stack.top+1; i++)
+    for (int i = 0; i < stack.top; i++)
     {
-        result.array[++result.top]=stack.array[i];
+        result.array[result.top++]=stack.array[i];
     }
     return result;
 }
 
 void clear(Stack *stack){
-    stack->top=-1;
+    stack->top=0;
 }
 
 void delete(Stack *vector){
-    vector->size=0;
     free(vector->array);
     vector->array=NULL;
-    vector->top=-1;
+    vector->top=0;
+    vector->size=0;
 }
 
 bool isEmpty(Stack stack){
-    return stack.top==-1;
+    return stack.top==0;
 }
 
 bool isFull(Stack stack){
-    return stack.top==stack.size-1;
-}
-
-int length(Stack stack){
-    return stack.top+1;
+    return stack.top==stack.size;
 }
 
 void push(Stack *stack, int value){
-    if(isFull(*stack)==1){
+    if(isFull(*stack)){
         printf("Stack Overflow\n");
         return;
     }
-    stack->array[++stack->top]=value;
+    stack->array[stack->top++]=value;
 }
 
 int peek(Stack stack){
-    if(isEmpty(stack)==1){
+    if(isEmpty(stack)){
         printf("Stack Underflow\n");
         return -1;
     }
-    return stack.array[stack.top];
+    return stack.array[stack.top-1];
 }
 
 int pop(Stack *stack){
-    if(isEmpty(*stack)==1){
+    if(isEmpty(*stack)){
         printf("Stack Underflow\n");
         return -1;
     }
-    return stack->array[stack->top--];
+    return stack->array[--stack->top];
 }
 
 void traverse(Stack stack){
     printf("[");
-    for (int i = 0; i < stack.top+1; i++)
+    for (int i = 0; i < stack.top; i++)
     {
         printf(" %d",stack.array[i]);
     }
-    printf(" ] : %d/%d\n",stack.top+1,stack.size);
+    printf(" ] : %d/%d\n",stack.top,stack.size);
 }
 
 void main()
 {
     Stack stq1=init(5);
-    push(&stq1,11);   
+    push(&stq1,11);
     push(&stq1,22);
     push(&stq1,33);
-    push(&stq1,44);   
-    push(&stq1,55);   
+    push(&stq1,44);
+    push(&stq1,55);
     Stack stq2=copy(stq1);
     traverse(stq1);
     traverse(stq2);
-    pop(&stq1);
+    printf("%d\n",pop(&stq1));
     traverse(stq1);
     traverse(stq2);
 }
